610b5f8666515787fc0b6eff38f8e9a984c9a032
rohitranjan15 <rohitranjan1233@gmail.com>
2024-06-23 00:56:43 +0530

    feat: Setup db connection

---
+++ server/global/config.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql2");
+
+// Create the connection to the database
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "chat_app",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = pool.promise();
