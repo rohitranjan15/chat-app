c132396443c33260b5df813fe6f3b66dfc1884ce
rohitranjan15 <rohitranjan1233@gmail.com>
2024-08-06 05:20:16 +0530

    feat: create middleware to validate access token

---
+++ server/src/middleware/middleware.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const SECRET_KEY = process.env.SECRET_KEY;
+
+const validateUserAccessToken = async (req, res, next) => {
+  try {
+    const token = req.headers["authorization"];
+
+    if (!token) {
+      return res.status(403).json({ message: "No token provided." });
+    }
+
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+      if (err) {
+        return res
+          .status(401)
+          .json({ message: "Failed to authenticate token." });
+      }
+
+      // If token is valid, save decoded info to request for use in other routes
+      req.user = decoded;
+      next();
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error." });
+  }
+};
+
+module.exports = validateUserAccessToken;
